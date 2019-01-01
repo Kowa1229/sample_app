0f7fa8ba796b782ca58b910f888c67c6e48dd973
Kowa Jia Liang <jlkowa1229@gmail.com>
2019-01-02 02:25:02 +0800

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name:  "Example User",
              email: "jlkowa1229@gmail.com",
              password:              "123123",
              password_confirmation: "123123",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -19,5 +21,7 @@ User.create!(name:  "Example User",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
